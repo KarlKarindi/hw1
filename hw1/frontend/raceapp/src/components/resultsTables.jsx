d63747c6eb2df90b177220665d366398001b1c26
KarlKarindi <karl_erik97@hotmail.com>
2019-06-28 19:16:08 +0300

    resultstables before reformat

--- hw1/frontend/raceapp/src/components/resultsTables.jsx
+++ hw1/frontend/raceapp/src/components/resultsTables.jsx
@@ -5,23 +5,27 @@ import { observer } from "mobx-react";
 @observer
 class ResultsTable extends Component {
   @observable results = [];
-  @observable readyForRace = false;
-  @observable athletesResults = {};
+  @observable idToResults = {};
+  @observable idToAthleteName = {};
   @observable timerToDisplayableFormat = {};
+  @observable readyForRace = false;
 
   componentDidMount() {
+    this.setAllIdToAthlete(toJS(this.props.athletes));
     this.generateTimingPoints(toJS(this.props.athletes));
+    this.readyForRace = true;
   }
 
-  // Shuffles athletes array so that times are given to athletes randomly.
-  shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  setAllIdToAthlete = athletesJSON => {
+    let i;
+    for (i = 0; i < athletesJSON.length; i++) {
+      this.idToAthlete[athletesJSON[i].id] = `${athletesJSON[i].firstName} ${
+        athletesJSON[i].secondName
+      }`;
     }
-    return array;
-  }
+  };
 
+  // Generating random timing points before the race. Then saving the times in a readable format.
   generateTimingPoints = athletes => {
     let id;
     for (id = 1; id < athletes.length + 1; id++) {
@@ -31,7 +35,7 @@ class ResultsTable extends Component {
         +(corridorTime + 3000);
       let result = [corridorTime, finishTime];
       this.results.push(result);
-      this.athletesResults[id] = result;
+      this.idToResults[id] = result;
 
       // Convert timer time, which is an int, to a displayable format.
       this.timerToDisplayableFormat[result[0]] = this.convertTimerToMinSecMsec(
@@ -41,9 +45,6 @@ class ResultsTable extends Component {
         result[1]
       );
     }
-
-    console.log(toJS(this.athletesResults));
-    this.readyForRace = true;
   };
 
   convertTimerToMinSecMsec = time => {
@@ -53,6 +54,7 @@ class ResultsTable extends Component {
     return `${minutes}:${seconds}:${cntsecs}`;
   };
 
+  // Used for returning the athlete id of specific results.
   getKey = (dict, val) => {
     for (let key in dict) {
       if (dict[key][0] === val[0] && dict[key][1] === val[1]) {
@@ -61,8 +63,22 @@ class ResultsTable extends Component {
     }
   };
 
+  print = () => {
+    console.log("lmao");
+  };
+
+  // Shuffles athletes array so that times are given to athletes randomly.
+  shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   render() {
     if (this.readyForRace === true) {
+      // corridorResults contains only times that the counter has gone over already.
+      // Then the results are sorted in descending order. Same with finishResults.
       let corridorResults = toJS(this.results).filter(
         corridorTime => this.props.timerTime > corridorTime[0]
       );
@@ -79,33 +95,48 @@ class ResultsTable extends Component {
 
       return (
         <React.Fragment>
-          <div className="rowA">
-            <div className="athletesTable m-4">
-              <h3 className="text-center">Finish corridor</h3>
-              {corridorResults.map(results => (
-                <li
-                  className="list-group-item d-flex justify-content-between align-items-center m-2"
-                  key={this.getKey(toJS(this.athletesResults), results)}
-                >
-                  {this.getKey(toJS(this.athletesResults), results)}{" "}
-                  {this.timerToDisplayableFormat[results[0]]}
-                  <span className="badge badge-info badge-pill m-2" />
-                </li>
-              ))}
+          <div className="tableRowB">
+            <div>
+              <div className="athletesResultsTable m-5">
+                <h3 className="text-center">Finish corridor</h3>
+                {corridorResults.map(results => (
+                  <li
+                    className="list-group-item d-flex justify-content-between align-items-center m-2"
+                    key={this.getKey(toJS(this.idToResults), results)}
+                  >
+                    <b>
+                      {
+                        this.idToAthlete[
+                          this.getKey(toJS(this.idToResults), results)
+                        ]
+                      }
+                    </b>{" "}
+                    {this.timerToDisplayableFormat[results[0]]}
+                    <span className="badge badge-info badge-pill m-2" />
+                  </li>
+                ))}
+              </div>
             </div>
-
-            <div className="athletesTable m-4">
-              <h3 className="text-center">Finish Line</h3>
-              {finishResults.map(results => (
-                <li
-                  className="list-group-item d-flex justify-content-between align-items-center m-2"
-                  key={this.getKey(toJS(this.athletesResults), results)}
-                >
-                  {this.getKey(toJS(this.athletesResults), results)}{" "}
-                  {this.timerToDisplayableFormat[results[1]]}
-                  <span className="badge badge-info badge-pill m-2" />
-                </li>
-              ))}
+            <div>
+              <div className="athletesResultsTable m-5">
+                <h3 className="text-center">Finish Line</h3>
+                {finishResults.map(results => (
+                  <li
+                    className="list-group-item d-flex justify-content-between align-items-center m-2"
+                    key={this.getKey(toJS(this.idToResults), results)}
+                  >
+                    <b>
+                      {
+                        this.idToAthlete[
+                          this.getKey(toJS(this.idToResults), results)
+                        ]
+                      }
+                    </b>{" "}
+                    {this.timerToDisplayableFormat[results[1]]}
+                    <span className="badge badge-info badge-pill m-2" />
+                  </li>
+                ))}
+              </div>
             </div>
           </div>
         </React.Fragment>
